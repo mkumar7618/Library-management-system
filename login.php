c4afcce55b6adfb7e3b2fa5255f16a92498cf3e0
mohit kumar <mkm9744@gmail.com>
2020-10-13 17:15:25 +0530

    Database added

--- login.php
+++ login.php
@@ -7,78 +7,76 @@ session_start();
 <head>
 
 <!--import head.html file -->
-<?php include('head.html'); ?>
+<?php include 'head.html';?>
 
 <link rel="stylesheet" href="loginstyle.css" />
 
 </head>
 <body>
 
-<?php include('nav.php'); ?>
+<?php include 'nav.php';?>
 <br>
 
 
 
 <div class = "container-fluid">
 	<div class="wrapper">
-		<form action="login.php" method="post" name="Login_Form" class="form-signin" enctype="multipart/form-data">       
+		<form action="login.php" method="post" name="Login_Form" class="form-signin" enctype="multipart/form-data">
 		    <h3 class="form-signin-heading">Library<br>Sign In Here</h3>
 			  <hr class="colorgraph">
 			<div class="form-group">
 			<input type="text" class="form-control" name="User" placeholder="Username" required autofocus="" />
 			</div>
 			<div class="form-group">
-			<input type="password" class="form-control" name="Password" placeholder="Password" required />     		  
+			<input type="password" class="form-control" name="Password" placeholder="Password" required />
 			</div>
 			<div class="form-group">
 			<button class="btn btn-lg btn-primary btn-block"  name="Submit" type="Submit">Login</button>
 			</div>
-		</form>		
+		</form>
 		<?php
-			if(isset($_POST['Submit'])){
-				$user = $_POST['User'];
-				$password = $_POST['Password'];	
-				$con = mysqli_connect('localhost','root','','library');
-				if($con){
-					$query = "select * from library_admins where admin_id='$user' AND admin_pass='$password'";
-					$result = mysqli_query($con,$query);
-					if(mysqli_num_rows($result)>0){
-						$_SESSION['user'] = $user;
-						$_SESSION['pass'] = $password;
-						echo "
+if (isset($_POST['Submit'])) {
+    $user = $_POST['User'];
+    $password = $_POST['Password'];
+    $con = mysqli_connect('localhost', 'root', '', 'library');
+    if ($con) {
+        $query = "select * from library_admins where admin_id='$user' AND admin_pass='$password'";
+        $result = mysqli_query($con, $query);
+        if (mysqli_num_rows($result) > 0) {
+            $_SESSION['user'] = $user;
+            $_SESSION['pass'] = $password;
+            echo "
 						<script>
 							window.location.href='http://localhost/library.com/library_admins/index.php';
 						</script>";
 
-					}
-					else{
-						$query = "select * from user_login where user_name='$user' AND user_pass='$password'";
-						$result = mysqli_query($con,$query);
-						if(mysqli_num_rows($result)>0){
-							$_SESSION['user'] = $user;
-							$_SESSION['pass'] = $password;
-							echo "
+        } else {
+            $query = "select * from user_login where user_name='$user' AND user_pass='$password'";
+            $result = mysqli_query($con, $query);
+            if (mysqli_num_rows($result) > 0) {
+                $_SESSION['user'] = $user;
+                $_SESSION['pass'] = $password;
+                echo "
 							<script>
 								window.location.href='http://localhost/library.com/library_users/index.php';
 						</script>";
 
-						}
-						else{
-							echo "<script>
+            } else {
+                echo "<script>
 								alert('You are not authenticated!');
 								</script>";
-						}
-					}
-				}
+            }
+        }
+    }
 
-			}
+}
 
-		?>
+?>
 	</div>
-	
+
 
 <div class="footer">
-	<?php include('footer.php'); ?>
+	<?php include 'footer.php';?>
 </div>
 
 </div>
